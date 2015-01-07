621d2c21ead700e79862221ee6324d2153b881ad
la2495514 <laquino4@student.rcc.edu>
2015-01-07 11:48:52 -0800

    Signed-off-by: la2495514 <laquino4@student.rcc.edu>

---
+++ Class_Lab/MathInstructorDisagreement_V2/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Lorenzo Aquino
+ * Created on January 7, 2015, 11:17 AM
+ * Purpose: How easy computers can make mistakes
+ */
+
+//system Libraries
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+//user libraries
+
+//Global Constant
+
+//FunctionPototype
+
+//Execution Begins here
+int main(int argc, char** argv) {
+    //Declare 3 Variables
+    float a,b,c;
+    //Prompt for a and b
+    cout<<"Input 2 float values"<<endl;
+    cin>>a>>b;
+    //Sum of the values
+    c=a+b;
+    //output Results
+    cout<<c<<" = "<<a<<" + "<<b<<endl;
+
+    return 0;
+}

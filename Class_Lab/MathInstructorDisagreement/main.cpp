3f679084a0753577886594008e3d795c2a854d6f
la2495514 <laquino4@student.rcc.edu>
2015-01-07 11:33:43 -0800

    Signed-off-by: la2495514 <laquino4@student.rcc.edu>

---
+++ Class_Lab/MathInstructorDisagreement/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Lorenzo Aquino
+ * Created on January 7, 2015, 11:17 AM
+ * Purpose: How easy computers can make mistakes
+ */
+
+//system Libraries
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+//user libraries
+
+//Global Constant
+
+//FunctionPototype
+
+//Execution Begins here
+int main(int argc, char** argv) {
+    //Declare 3 Variables
+    short a,b,c;
+    //Prompt for a and b
+    cout<<"Input 2 short values"<<endl;
+    cin>>a>>b;
+    //Sum of the values
+    c=a+b;
+    //output Results
+    cout<<c<<" = "<<a<<" = "<<b<<endl;
+
+    return 0;
+}
+

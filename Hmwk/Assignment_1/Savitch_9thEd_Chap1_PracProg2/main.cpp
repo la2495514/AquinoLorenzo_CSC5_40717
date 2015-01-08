844baaafd7d7f00d0f3493caa539ea66b137c434
la2495514 <laquino4@student.rcc.edu>
2015-01-08 11:54:10 -0800

    Signed-off-by: la2495514 <laquino4@student.rcc.edu>

---
+++ Hmwk/Assignment_1/Savitch_9thEd_Chap1_PracProg2/main.cpp
@@ -0,0 +1,41 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on January 8, 2015, 11:45 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+
+ int main (int argc, char** argv) {
+     
+    int number_of_pods, peas_per_pod, total_peas;
+ cout << "Hello!"<< endl;   
+ cout << "Press return after entering a number.\n";
+ cout << "Enter the number of pods:\n";
+ 
+ cin >> number_of_pods;
+ 
+ cout << "Enter the number of peas in a pod:\n";
+ cin >> peas_per_pod;
+ total_peas = number_of_pods * peas_per_pod;
+ cout << "If you have ";
+ cout << number_of_pods;
+ cout << " pea pods\n";
+ cout << "and ";
+ cout << peas_per_pod;
+ cout << " peas in each pod, then\n";
+ cout << "you have ";
+ cout << total_peas;
+ cout << " peas in all the pods.\n";
+ cout << "Good-Bye!\n";
+
+    return 0;
+ }
+

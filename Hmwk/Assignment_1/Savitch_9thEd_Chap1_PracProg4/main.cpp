f29ae8406059a3f8ed6f93b83a3b778e2d9a71ef
la2495514 <laquino4@student.rcc.edu>
2015-01-11 19:42:23 -0800

    Signed-off-by: la2495514 <laquino4@student.rcc.edu>

---
+++ Hmwk/Assignment_1/Savitch_9thEd_Chap1_PracProg4/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Mayurin_rin
+ *
+ * Created on January 11, 2015, 7:23 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+
+ int main (int argc, char** argv) {
+     
+    int number_of_pods, peas_per_pod, total_peas;
+ cout << "Press return after entering a number.\n";
+ cout << "Enter the number of pods:\n";
+ 
+ cin >> number_of_pods;
+ 
+ cout << "Enter the number of peas in a pod:\n";
+ cin >> peas_per_pod;
+ total_peas = number_of_pods + peas_per_pod;//logic error
+ cout << "If you have ";
+ cout << number_of_pods;
+ cout << " pea pods\n";
+ cout << "and ";
+ cout << peas_per_pod;
+ cout << " peas in each pod, then\n";
+ cout << "you have ";
+ cout << total_peas;
+ cout << " peas in all the pods.\n";
+
+    return 0;
+}
+
